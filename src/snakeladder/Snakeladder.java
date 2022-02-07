ca06cb1df5b5419fee32a6f12daee7d6ba1a56b0
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-07 20:55:19 +0530

    Added: NoPlay Snake Ladder

--- src/snakeladder/Snakeladder.java
+++ src/snakeladder/Snakeladder.java
@@ -1,12 +1,42 @@
 package snakeladder;
 
+import java.util.Random;
+
 public class Snakeladder {
 
-	public static void main(String[] args) {
-		int position=0;
-			System.out.println("Intial Position " + position);
-			
-			int player1 = (int)(Math.floor(Math.random() * 10) % 6) +1;
-			System.out.println("Dice rolled value: " + player1);
+	//class or static variable
+	static final int Snake = 2;
+	static final int ladder = 1;
+	static final int Noplay = 0;
+
+	public static void main(String[] args) 
+	{
+		int position = 0;
+		Random ran = new Random();
+		int dice = ran.nextInt(6)+1;
+		System.out.println("dice: "+dice);
+		
+		int optionCheck = ran.nextInt(3);
+		System.out.println("optionCheck: "+optionCheck);
+		
+		if (optionCheck == ladder) 
+		{
+			System.out.println("Ladder");
+			position = position + dice;
+		}
+		
+		else if (optionCheck == Snake) 
+		{
+			System.out.println("Snake");
+			position = position - dice;
+		}
+		else {
+			System.out.println("No Play");
+		}
+		if (position<0)
+			position=0;
+		System.out.println("position: "+position);
 	}
+
 }
+
