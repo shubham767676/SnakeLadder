f7a62012d89b69fe907a09c4c989c0b53c752ed8
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-07 20:49:28 +0530

    Added: DiceRolled

--- src/snakeladder/Snakeladder.java
+++ src/snakeladder/Snakeladder.java
@@ -5,5 +5,8 @@ public class Snakeladder {
 	public static void main(String[] args) {
 		int position=0;
 			System.out.println("Intial Position " + position);
+			
+			int player1 = (int)(Math.floor(Math.random() * 10) % 6) +1;
+			System.out.println("Dice rolled value: " + player1);
 	}
 }
